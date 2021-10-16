aabc91cacf59070dc6b7aacdfd9fc164f14bafe5
Maciej Lewandowski <maciejlewandowski5@gmail.com>
2021-10-16 12:36:13 +0200

    CU-1my3n9q - Add camera permison check

--- app/src/main/java/com/example/forestescape/MainActivity.kt
+++ app/src/main/java/com/example/forestescape/MainActivity.kt
@@ -4,21 +4,43 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.lifecycle.ViewModelProvider
 import com.google.ar.core.ArCoreApk
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var permissionsViewModel: PermissionsViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        finishIfARNotAvaliable();
+        finishIfArCoreNotAvailable();
+
+        permissionsViewModel = ViewModelProvider(this).get(PermissionsViewModel::class.java)
+        finishIfPermissionIsDeniedNTimes(3)
     }
 
-    private fun finishIfARNotAvaliable() {
+    private fun finishIfPermissionIsDeniedNTimes(n:Int) {
+        permissionsViewModel.getCanceled().observe(this) {
+            if (it == n) {
+                this.finish()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!permissionsViewModel.hasCameraPermission(this)) {
+            permissionsViewModel.requestPermission()
+        }
+        return
+    }
+
+    private fun finishIfArCoreNotAvailable() {
         val availability = ArCoreApk.getInstance().checkAvailability(this)
         if (availability.isTransient) {
             Handler(Looper.getMainLooper()).postDelayed({
-                finishIfARNotAvaliable()
+                finishIfArCoreNotAvailable()
             }, 200)
         }
         if (!availability.isSupported) {
@@ -26,3 +48,5 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
+
